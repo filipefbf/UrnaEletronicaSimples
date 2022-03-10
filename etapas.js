adaf3e4bb7ced57e25e62e9ecce75cb1ef95407f
filipefbf <filipe_fbf@hotmail.com>
2022-03-10 13:46:42 -0300

    continuação do projeto

---
+++ etapas.js
@@ -0,0 +1,48 @@
+let etapas = [
+    {
+      titulo: "VEREADOR",
+      numeros: 5,
+      candidatos: [
+        {
+            numero: "38111",
+            nome: "Roberto Silva",
+            partido: "QSL",
+            fotos: [
+                { url:'38111.jpg', legenda: 'Vereador' }
+            ],  
+        },
+        {
+          numero: "77222",
+          nome: "Jadson Barbosa K.",
+          partido: "UNIDOS",
+          fotos: [{ url:"77222.jpg", legenda: "Vereador" }],
+        },
+      ],
+    },
+    {
+      titulo: "PREFEITO",
+      numeros: 2,
+      candidatos: [
+        {
+          numero: "99",
+          nome: "Orivaldo Santos Jr.",
+          partido: "QSL",
+          vice: "Cícero S.",
+          fotos: [
+            { url: "99.jpg", legenda: "Prefeito" },
+            { url: "99_2.jpg", legenda: "Vice-Prefeito", small: true },
+          ],
+        },
+        {
+          numero: "84",
+          nome: "Carlos Suarez G.",
+          partido: "QWERTY",
+          vice: "Bonieky L.",
+          fotos: [
+            { url: "84.jpg", legenda: "Prefeito" },
+            { url: "84_2.jpg", legenda: "Vice-Prefeito", small: true },
+          ],
+        },
+      ],
+    },
+  ];
